aa3398daea6efdd6b39a1175db4bbbf3fe2ff28e
Greenish420 <119529693+Greenish420@users.noreply.github.com>
2025-11-03 21:23:07 +0300

    V 1.2

--- index.js
+++ index.js
@@ -1,183 +1,167 @@
-const display = document.getElementById('display');
+const display = document.getElementById("display");
 let readOnly = false;
+let fullnumber = false;
 const buttonValues = [
-    "AC", "+/-", "%", "÷",
-    "7", "8", "9", "×",
-    "4", "5", "6", "-",
-    "1", "2", "3", "+",
-    "0", ".", "="
+  "AC",
+  "+/-",
+  "%",
+  "÷",
+  "7",
+  "8",
+  "9",
+  "×",
+  "4",
+  "5",
+  "6",
+  "-",
+  "1",
+  "2",
+  "3",
+  "+",
+  "0",
+  ".",
+  "=",
 ];
 const topValues = ["AC", "+/-", "%"];
 const rightValues = ["÷", "×", "-", "+", "="];
-const buttons_container = document.getElementById('buttons_container');
+const buttons_container = document.getElementById("buttons_container");
 let A = 0;
 let operator = null;
 let B = null;
 
-
 for (let i = 0; i < buttonValues.length; i++) {
-    const btn = document.createElement("button");
-    btn.textContent = buttonValues[i];
-    if (buttonValues[i] == "0") {
-        btn.style.gridColumn = "span 2";
-        btn.style.width = "6em";
-    }
-    else if (topValues.includes(buttonValues[i])) {
-        btn.style.backgroundColor = "white";
-        btn.style.color = "black";
-    }
-    else if (rightValues.includes(buttonValues[i])) {
-        btn.style.backgroundColor = "orange";
-    }
-    btn.addEventListener("click", function () {
-        if (btn.textContent == "0") {
-            if (!readOnly) {
-                if (display.value != "0")
-                    display.value += "0";
-            }
-            else {
-                display.value = '';
-                if (display.value != "0")
-                    display.value += "0";
-            }
+  const btn = document.createElement("button");
+  btn.textContent = buttonValues[i];
+  if (buttonValues[i] == "0") {
+    btn.style.gridColumn = "span 2";
+    btn.style.width = "6em";
+  } else if (topValues.includes(buttonValues[i])) {
+    btn.style.backgroundColor = "white";
+    btn.style.color = "black";
+  } else if (rightValues.includes(buttonValues[i])) {
+    btn.style.backgroundColor = "orange";
+  }
+  btn.addEventListener("click", function () {
+    if (btn.textContent == "0") {
+      if (!readOnly) {
+        if (display.value != "0") display.value += "0";
+      } else {
+        display.value = "";
+        if (display.value != "0") display.value += "0";
+      }
+    } else if (btn.textContent == ".") {
+      if (!readOnly) {
+        if (!display.value.includes(".")) {
+          if (display.value != "") {
+            display.value += ".";
+          } else {
+            display.value += "0";
+            display.value += ".";
+          }
         }
-        else if (btn.textContent == ".") {
-            if (!readOnly) {
-                if (!display.value.includes('.')) {
-                    if (display.value != "") {
-                        display.value += '.';
-                    }
-                    else {
-                        display.value += "0";
-                        display.value += ".";
-                    }
-
-                }
-            }
-            else {
-                display.value = '';
-                if (!display.value.includes('.')) {
-                    if (display.value != "") {
-                        display.value += '.';
-                    }
-                    else {
-                        display.value += "0";
-                        display.value += ".";
-                    }
-
-                }
-            }
+      } else {
+        display.value = "";
+        if (!display.value.includes(".")) {
+          if (display.value != "") {
+            display.value += ".";
+          } else {
+            display.value += "0";
+            display.value += ".";
+          }
         }
-        else if (topValues.includes(btn.textContent)) {
-            if (btn.textContent == topValues[0]) {
-                readOnly = false;
-                display.value = '';
-                clearAll();
-            }
-            else if (btn.textContent == topValues[1]) {
-                if (display.value != '') {
-                    if (display.value[0] != '-') {
-                        display.value = '-' + display.value;
-                    }
-                    else {
-                        display.value = display.value.slice(1);
-                    }
-                }
-            }
-            else {
-
-                display.value = Number(display.value) / 100;
-
-            }
+      }
+    } else if (topValues.includes(btn.textContent)) {
+      if (btn.textContent == topValues[0]) {
+        readOnly = false;
+        display.value = "";
+        clearAll();
+      } else if (btn.textContent == topValues[1]) {
+        if (display.value != "") {
+          if (display.value[0] != "-") {
+            display.value = "-" + display.value;
+          } else {
+            display.value = display.value.slice(1);
+          }
         }
-        else if (rightValues.includes(btn.textContent)) {
-            if (btn.textContent != '=') {
-                readOnly = false;
-                if (A != 0) {
-                    B = Number(display.value);
-                    display.value = '';
-                    calculate();
-                    clearAll();
-                    operator = btn.textContent;
-                    readOnly = true;
-                    A = Number(display.value);
-                }
-                else if (A == 0) {
-                    if (operator == null) {
-                        A = Number(display.value);
-                        display.value = "";
-                        operator = btn.textContent;
-                    }
-                }
-            }
-            else {
-                if (!readOnly) {
-                    if (operator != null) {
-
-
-                        B = Number(display.value);
-                        calculate();
-                        readOnly = true;
-                    }
-                }
-                else{
-                    B = Number(display.value);
-                    display.value = '';
-                    calculate();
-                    clearAll();
-                    readOnly = true;
-                    
-                }
-            }
-
-
+      } else {
+        display.value = Number(display.value) / 100;
+      }
+    } else if (rightValues.includes(btn.textContent)) {
+      if (btn.textContent != "=") {
+        readOnly = false;
+        if (A != 0) {
+          B = Number(display.value);
+          display.value = "";
+          calculate();
+          clearAll();
+          operator = btn.textContent;
+          readOnly = true;
+          fullnumber = true;
+          A = Number(display.value);
+        } else if (A == 0) {
+          if (operator == null) {
+            A = Number(display.value);
+            display.value = "";
+            operator = btn.textContent;
+          }
+        }
+      } else {
+        if (!readOnly) {
+          if (operator != null) {
+            B = Number(display.value);
+            calculate();
+            readOnly = true;
+            fullnumber = true;
+          }
         } else {
-            if (!readOnly) {
-                display.value += btn.textContent;
-            }
-            else {
-                display.value = '';
-                display.value += btn.textContent;
-            }
-
+          B = Number(display.value);
+          display.value = "";
+          calculate();
+          clearAll();
+          readOnly = true;
+          fullnumber = true;
         }
-    });
-    buttons_container.appendChild(btn);
+      }
+    } else {
+      if (readOnly && fullnumber) {
+        display.value = "";
+        display.value += btn.textContent;
+        fullnumber = false;
+      } else {
+        display.value += btn.textContent;
+      }
+    }
+  });
+  buttons_container.appendChild(btn);
 }
 
 function clearAll() {
-    A = 0;
-    operator = null;
-    B = null;
+  A = 0;
+  operator = null;
+  B = null;
 }
 
 function calculate() {
+  switch (operator) {
+    case "+":
+      display.value = A + B;
 
-    switch (operator) {
-        case '+':
+      break;
 
-            display.value = A + B;
+    case "-":
+      display.value = A - B;
 
-            break;
+      break;
 
-        case '-':
+    case "×":
+      display.value = A * B;
 
-            display.value = A - B;
+      break;
 
-            break;
+    case "÷":
+      display.value = A / B;
 
-        case '×':
-
-            display.value = A * B;
-
-            break;
-
-        case '÷':
-
-            display.value = A / B;
-
-            break;
-    }
-    clearAll();
-
+      break;
+  }
+  clearAll();
 }
